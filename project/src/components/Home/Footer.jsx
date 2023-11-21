3d94d5e47df228b46b4884118f74dea4735e3eb7
MrFutureBoss <146051100+MrFutureBoss@users.noreply.github.com>
2023-11-21 16:59:02 +0700

    alo

---
+++ project/src/components/Home/Footer.jsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container fluid className="footer-container">
+      <Row>
+        <Col xs={12}>
+           
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
