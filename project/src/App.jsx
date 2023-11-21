8e586c4fc0d7cace622051d8609023d48043f543
tranlam1z1 <127399721+tranlam1z1@users.noreply.github.com>
2023-11-21 16:51:23 +0700

    Create App.jsx

---
+++ project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+const App = () => {
+  return (
+    <div>
+      <SignIn>
+      
+    </SignIn>
+    <SignUp>
+      
+    </SignUp>
+    </div>
+    
+  );
+};
+
+export default App;
