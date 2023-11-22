af898de2e5217646c8c72178fcb4f93121271f43
tranlam1z1 <127399721+tranlam1z1@users.noreply.github.com>
2023-11-22 13:24:59 +0700

    Update App.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,34 +1,20 @@
 
 import "./App.css";
 import Home from "./components/Home/Home";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/register" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
-=======
-import React from 'react';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-
-const App = () => {
-  return (
-    <div>
-      <SignIn>
-      
-    </SignIn>
-    <SignUp>
-      
-    </SignUp>
-    </div>
-    
-  );
-};
 
 
 export default App;
